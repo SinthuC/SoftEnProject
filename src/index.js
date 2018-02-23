70c3c848f4e007efe28499988e40c4d1190ba69f
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-24 02:09:32 +0700

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Provider,
+} from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import Router from './Router';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
