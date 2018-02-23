00921f597dd66a535f56e6fc462cd2f512f1cd9c
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-23 15:53:37 +0700

    Delete unused import

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -6,38 +6,31 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   Container,
-  Button 
+  Button
 } from 'reactstrap';
 
 class NavBar extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       isOpen: false
     }
   }
   render() {
     return (
-      <Navbar  dark expand style={{backgroundColor:'green'}}>
-      <Container>
-      <NavbarBrand href="/">Test</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-     
-            <NavItem>
-            <Button color="success">Sign in</Button> 
-            </NavItem>
-             
-          </Nav>
-        </Collapse>
-      </Container>
+      <Navbar dark expand style={{ backgroundColor: 'green', marginBottom: 16 }}>
+        <Container>
+          <NavbarBrand href="/">Test</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Button color="success">Sign in</Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
       </Navbar>
     );
   }
