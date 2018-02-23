106bb01650184192ef9e21b502b74a71d23d43bd
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-24 02:07:00 +0700

    Add signinmodal,  signupmodal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React  from 'react';
 import NavBar from './layout/NavBar';
+import SignUpModal from './layout/SignUpModal';
+import SignInModal from './layout/SignInModal';
 const App = ({children}) => (
   <div>
     <NavBar />
     {children}
+    <SignUpModal />
+    <SignInModal />
   </div>
 );
 
