b21760bd3abba9e1cb0d94d439a76a47dfdb15ee
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-24 02:08:56 +0700

    Connect redux

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -9,6 +9,11 @@ import {
   Container,
   Button
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import {
+  toggleSignUp,
+  toggleSignIn,
+} from '../redux/action/auth';
 
 class NavBar extends React.Component {
   constructor() {
@@ -16,24 +21,38 @@ class NavBar extends React.Component {
     this.state = {
       isOpen: false
     }
+    this.toggle = this.toggle.bind(this);
   }
+
+  toggle(isOpen) {
+    console.log(isOpen);
+    this.setState({ isOpen: !isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
+    const { toggle } = this;
     return (
-      <Navbar dark expand style={{ backgroundColor: 'green', marginBottom: 16 }}>
-        <Container>
-          <NavbarBrand href="/">Test</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <Button color="success">Sign in</Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
+      <div>
+        <Navbar dark expand="md" style={{ backgroundColor: 'green', marginBottom: 16 }}>
+          <Container>
+            <NavbarBrand href="/" className="mr-auto">Test</NavbarBrand>
+            <NavbarToggler onClick={() => toggle(isOpen)} className="mr-2" />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem className="m-2">
+                  <Button outline color="success" onClick={() => this.props.toggleSignIn()}>Sign In</Button>
+                </NavItem>
+                <NavItem className="m-2">
+                  <Button outline color="success" onClick={() => this.props.toggleSignUp()}>Sign Up</Button>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
   }
 }
 
-export default NavBar;
+export default connect(null, { toggleSignUp, toggleSignIn })(NavBar);
