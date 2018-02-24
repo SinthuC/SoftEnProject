07ec3a1fd590a1bf5ba80d49f25487b928171031
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-24 19:35:34 +0700

    Add admin

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter as AppRouter, Route, Switch } from 'react-router-dom';
 
 import App from './App';
 import Home from './page/Home';
+import Admin from './page/Admin';
 
 const Router = () => (
   <AppRouter>
     <App>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/admin" component={Admin} />
       </Switch>
     </App>
   </AppRouter>
