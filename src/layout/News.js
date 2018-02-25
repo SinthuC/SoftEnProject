3817649a9ed36099ef3188f692b1d66da25ec49c
SinthuC <sinthu_ch@kkumail.com>
2018-02-25 19:15:47 +0700

    Edit News

--- src/layout/News.js
+++ src/layout/News.js
@@ -10,15 +10,14 @@ import './News.css';
 
 
 const News = props => (
-        <ListGroupItem className="list-item">
+        <ListGroupItem className="list-item" style={{backgroundColor: 'rgba(144, 149, 93, 0.3)',color:'#283227'}}>
             <ListGroupItemHeading>{props.title}</ListGroupItemHeading>
-            <ListGroupItemText>{props.detail}</ListGroupItemText>
         </ListGroupItem>
 );
 
 News.propTypes = {
-    title: PropTypes.string,
-    detail: PropTypes.string
+    title: PropTypes.string,    
+   
 } 
 
 export default News;
