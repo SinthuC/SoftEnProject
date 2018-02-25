f4acb6eb8bbc02b4d597f7e916ed17edc0070369
SinthuC <sinthu_ch@kkumail.com>
2018-02-25 19:15:28 +0700

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React  from 'react';
 import NavBar from './layout/NavBar';
 import SignUpModal from './layout/SignUpModal';
 import SignInModal from './layout/SignInModal';
+import Footer from './layout/Footer';
 const App = ({children}) => (
-  <div>
+  <div style={{background: 'linear-gradient(#d5d18a, #f9f6c2)'}}>
     <NavBar />
     {children}
     <SignUpModal />
     <SignInModal />
+    <Footer />
   </div>
 );
 
