a340581a32bb962c98c8b594894e72e0c0cc4fbb
SinthuC <sinthu_ch@kkumail.com>
2018-02-25 19:16:47 +0700

    Add Admin page

---
+++ src/page/Admin.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+} from 'reactstrap';
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <Container fluid style={{marginTop: 16, marginBottom: 16}}>
+        <Row>
+          <Col lg={2} md={4} sm={12} xs={12}>
+            <ListGroup>
+              <ListGroupItem>Menu 1</ListGroupItem>
+              <ListGroupItem>Menu 2</ListGroupItem>
+              <ListGroupItem>Menu 3</ListGroupItem>
+              <ListGroupItem>Menu 4</ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg={10} md={8} sm={12} xs={12}>
+            Content here
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Admin;
