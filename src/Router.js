7d58cdb80422f10128860672cb9608ffb8f8c082
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-02-26 16:57:28 +0700

    Update router

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter as AppRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import App from './App';
 import Home from './page/Home';
 import Admin from './page/Admin';
+import Page403 from './page/403';
+import Page404 from './page/404';
 
-const Router = () => (
+const Router = props => (
   <AppRouter>
     <App>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/admin" component={Admin} />
+        <Route exact path="/admin" component={props.auth.user.admin ? Admin : Page403} />
+        <Route exact path="*" component={Page404} />
       </Switch>
     </App>
   </AppRouter>
 );
 
-export default Router;
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, null)(Router);
