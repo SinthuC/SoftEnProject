776535ab9a7b73098ee1905f3f49a1f8ee772ae4
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-03-03 17:01:53 +0700

    Update router

--- src/Router.js
+++ src/Router.js
@@ -9,12 +9,12 @@ import Page403 from './page/403';
 import Page404 from './page/404';
 
 const Router = props => (
-  <AppRouter basename="http://10.199.66.227/SoftEn2018/Sec01_NMB">
+  <AppRouter>
     <App>
       <Switch>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-        <Route exact path={`${process.env.PUBLIC_URL}/403`} component={Page403} />
-        <Route exact path={`${process.env.PUBLIC_URL}/*`} component={Page404} />
+        <Route exact path={`/`} component={Home} />
+        <Route exact path={`/403`} component={Page403} />
+        <Route exact path={`*`} component={Page404} />
       </Switch>
     </App>
   </AppRouter>
