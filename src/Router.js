60b5dced7dd5ffa75b7f1048f221d5a972636932
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-03-20 01:12:28 +0700

    Update Router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { HashRouter as AppRouter, Route, Switch } from 'react-router-dom';
-
+import { HashRouter as AppRouter } from 'react-router-dom';
+import { Route, Switch, } from 'react-router';
 
 import App from './App';
 import Home from './page/Home';
@@ -8,19 +8,20 @@ import Admin from './page/Admin';
 import Page403 from './page/403';
 import Page404 from './page/404';
 import Register from './page/Register';
-const Router = props => (
-  <AppRouter>
 
+const Router = props => (
+  <AppRouter basename={`${process.env.PUBLIC_URL}/`}>
     <Switch>
       <App>
-        <Route exact path={`/`} component={Home} />
-        <Route exact path={`/admin`} component={Admin} />
-        <Route exact path={`/register`} component={Register} />
-        <Route exact path={`/403`} component={Page403} />
-       
+        <Switch>
+          <Route exact path={`/`} component={Home} />
+          <Route exact path={`/admin`} component={Admin} />
+          <Route exact path={`/register`} component={Register} />
+          <Route exact path={`/403`} component={Page403} />
+          <Route exact path={`/*`} component={Page404} />
+        </Switch>
       </App>
     </Switch>
-
   </AppRouter>
 );
 
