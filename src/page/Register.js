c3b7c70084955b206e039e51efc09138c4dd6245
SinthuC <sinthu_ch@kkumail.com>
2018-03-20 02:02:26 +0700

    Add Regex to Register

--- src/page/Register.js
+++ src/page/Register.js
@@ -38,9 +38,47 @@ const styles = {
   },
 };
 
-//import { connect } from 'react-redux';
 
 class Register extends React.Component {
+  checkForm(eleID) {
+    var alertBox = eleID + 'Alert'
+    var inp = document.getElementById(eleID);
+    if (!inp.checkValidity()) {
+      document.getElementById(alertBox).innerHTML = '<div class="alert alert-danger" role="alert">' + inp.validationMessage + '</div>';
+    } else {
+      document.getElementById(alertBox).innerHTML = "";
+    }
+
+
+  }
+
+  checkPassword(){
+    if(document.getElementById('password').value!=document.getElementById('Cpassword').value){
+      document.getElementById('CpasswordAlert').innerHTML = '<div class="alert alert-danger" role="alert">Password not match</div>';
+      
+    }else{
+      document.getElementById('CpasswordAlert').innerHTML="";
+      document.getElementById('submit').disabled = false;
+    }
+  }
+
+  checkSubmit(){
+    var elements= ['name','pid','pidPic','username','password','Cpassword','ans1','ans2','ans3','email'];
+    var eleLength = elements.length;
+
+    for(var i=0;i<eleLength;i++){
+      var alertBox = elements + 'Alert'
+    var inp = document.getElementById(elements);
+    if (!inp.checkValidity()) {
+      document.getElementById(alertBox).innerHTML = '<div class="alert alert-danger" role="alert">' + inp.validationMessage + '</div>';
+    } else {
+      document.getElementById(alertBox).innerHTML = "";
+    }
+
+
+    }
+
+  }
   render() {
     return (
       <div>
@@ -49,99 +87,109 @@ class Register extends React.Component {
             <Col lg={8} style={styles.colSection}>
               <Container>
                 <Form>
+
                   <FormGroup>
-                    <Label for="username">Firstname - Lastname*</Label>
-                    <Input type="text" name="username" id="username" />
+                    <Label for="name">Firstname - Lastname*</Label>
+                   
+                    <div id="nameAlert"></div>
+
+                    <Input type="text" name="name" id="name" onBlur={() => this.checkForm('name')} pattern="\D{1,40}" required />
                   </FormGroup>
+
                   <FormGroup>
+
                     <Label for="pid">Personal ID / Passport Number*</Label>
-                    <Input type="text" name="pid" id="pid" />
+                    <div id="pidAlert"></div>
+                    <Input type="text" name="pid" id="pid" onBlur={() => this.checkForm('pid')}pattern="\d{9,13}" required/>
                   </FormGroup>
 
                   <FormGroup>
                     <Label for="pidPic">Upload copy of  ID card / Passport</Label>
-                    <Input type="file" name="pidPic" id="pidPic" accept='image/*' />
+                    <div id="pidPicAlert"></div>
+                    <Input type="file" name="pidPic" id="pidPic" accept='image/*' required/>
                     <FormText color="muted">
                       Upload your copy of ID card or Passport
           </FormText>
                   </FormGroup>
                   <FormGroup>
                     <Label for="username">Username*</Label>
-                    <Input type="text" name="username" id="username" />
+                    <div id="usernameAlert"></div>
+                    <Input type="text" name="username" id="username" onBlur={() => this.checkForm('username')} pattern="[A-Za-z0-9]{1,20}" required/>
                   </FormGroup>
                   <FormGroup>
-                    <Label for="Cpassword">Password*</Label>
-                    <Input type="password" name="password" id="password" />
+                    <Label for="password">Password*</Label>
+                    <div id="passwordAlert"></div>
+                    <Input type="password" name="password" id="password" onBlur={() => this.checkForm('password')} pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?-)(?=.*?_).{16,}$" required/>
                   </FormGroup>
                   <FormGroup>
                     <Label for="Cpassword">Confirm Password*</Label>
-                    <Input type="password" name="Cpassword" id="Cpassword" />
+                    <div id="CpasswordAlert"></div>
+                    <Input type="password" name="Cpassword" id="Cpassword" onBlur={this.checkPassword}/>
                   </FormGroup>
                   <FormGroup>
-                    <Label for="birthdate*">Birthdate</Label>
-                    <Input type="date" name="birthdate" id="birthdate" />
+                  
+                    <Label for="birthdate">Birthdate*</Label>
+                    <div id="birthdateAlert"></div>
+                    <Input type="date" name="birthdate" id="birthdate" required />
                   </FormGroup>
                   <Col lg={8}>
-                  <FormGroup>
+                    <FormGroup>
 
-                    
+
                       <Row style={styles.moreButton}>
                         <Label for="remind1">Select 1st Reminding question</Label>
-                        <Input type="select" name="remind1" id="remind1">
+                        <Input type="select" name="remind1" id="remind1" >
                           <option>วงดนตรีที่คุณชื่นชอบคือวงอะไร</option>
                           <option>เพื่อนที่สนิทที่สุดชื่ออะไร</option>
                           <option>สัตว์เลี้ยงตัวแรกของคุณชื่ออะไร</option>
-                          <option>ชื่อโรงเรียนประถมของคุณชื่ออะไร</option>
-                          <option>พ่อคุณชื่ออะไร</option>
-                          <option>พ่อคุณชื่ออะไร</option>
                         </Input>
-
-                        <Input type="text" name="ans1" id="ans1" />
+                        <div id="ans1Alert"></div>
+                        <Input type="text" name="ans1" id="ans1" onBlur={() => this.checkForm('ans1')} required />
                       </Row>
                       <Row style={styles.moreButton}>
                         <Label for="remind2">Select 2nd Reminding question</Label>
-                        <Input type="select" name="remind2" id="remind2">
-                          <option>วงดนตรีที่คุณชื่นชอบคือวงอะไร</option>
-                          <option>เพื่อนที่สนิทที่สุดชื่ออะไร</option>
-                          <option>สัตว์เลี้ยงตัวแรกของคุณชื่ออะไร</option>
+                        <Input type="select" name="remind2" id="remind2" >
+
                           <option>ชื่อโรงเรียนประถมของคุณชื่ออะไร</option>
                           <option>พ่อคุณชื่ออะไร</option>
-                          <option>พ่อคุณชื่ออะไร</option>
+                          <option>อาหารที่ชอบรับประทานคืออะไร</option>
                         </Input>
-                        <Input type="text" name="ans2" id="ans2" />
+                        <div id="ans2Alert"></div>
+                        <Input type="text" name="ans2" id="ans2" onBlur={() => this.checkForm('ans2')} required/>
                       </Row>
                       <Row style={styles.moreButton}>
                         <Label for="remind3">Select 3rd Reminding question</Label>
-                        <Input type="select" name="remind3" id="remind3">
-                          <option>วงดนตรีที่คุณชื่นชอบคือวงอะไร</option>
-                          <option>เพื่อนที่สนิทที่สุดชื่ออะไร</option>
-                          <option>สัตว์เลี้ยงตัวแรกของคุณชื่ออะไร</option>
-                          <option>ชื่อโรงเรียนประถมของคุณชื่ออะไร</option>
-                          <option>พ่อคุณชื่ออะไร</option>
-                          <option>พ่อคุณชื่ออะไร</option>
+                        <Input type="select" name="remind3" id="remind3" >
+                        <div id="Alert"></div>
+                          <option>จังหวัดที่ชอบไปคือจังหวัดใด</option>
+                          <option>แฟนคนแรกของคุณชื่ออะไร</option>
+                          <option>สัตว์ที่คุณเกลียดมากที่สุดคือสัตว์ชนิดใด</option>
+                      
                         </Input>
-                        <Input type="text" name="ans3" id="ans3" />
+                        <div id="ans3Alert"></div>
+                        <Input type="text" name="ans3" id="ans3" onBlur={() => this.checkForm('ans3')} required />
                       </Row>
-                    
-                  </FormGroup>
+
+                    </FormGroup>
                   </Col>
                   <FormGroup>
-                    
-                      <Label for="email">E-mail*</Label>
-                      <Input type="email" name="email" id="email"/>
-                    
-                    
-          </FormGroup>
-          <Button color="success" type="submit" id="submit" >Submit</Button>
-        </Form>
-        </Container>
-          </Col>
-        </Row>
+
+                    <Label for="email">E-mail*</Label>
+                    <div id="emailAlert"></div>
+                    <Input type="email" name="email" id="email" onBlur={() => this.checkForm('email')} required/>
+
+
+                  </FormGroup>
+                  <Button color="success" type="submit" id="submit" onClikc={this.checkSubmit}>Submit</Button>
+                </Form>
+              </Container>
+            </Col>
+          </Row>
         </Container>
       </div>
-          )
-        }
-      }
-      
-      
+    )
+  }
+}
+
+
 export default Register;
