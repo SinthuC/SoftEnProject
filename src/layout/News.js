e088d2f161ce01f8c70eb2ee312ad4e7df75df52
SinthuC <sinthu_ch@kkumail.com>
2018-03-21 14:01:52 +0700

    Add newsID to News

--- src/layout/News.js
+++ src/layout/News.js
@@ -9,7 +9,7 @@ import './News.css';
 
 const News = props => (
     <ListGroupItem className="list-item" style={{ backgroundColor: 'rgba(255, 255, 255, 0.15)', display: 'fixed', justifyContent: 'flex-start', alignItems: 'center' }}>
-        <div>
+        <div id={`news-${props.id}`}>
             <a href="#" className="list-link">
                 <h4 style={{ wordWrap: 'break-word' }}>{props.topic}</h4>
             </a>
