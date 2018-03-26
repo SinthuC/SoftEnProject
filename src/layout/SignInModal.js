3cb8c976b212efa32ff934ea7864453dc6482888
SinthuC <sinthu_ch@kkumail.com>
2018-03-26 19:02:54 +0700

    Update SignInModal

--- src/layout/SignInModal.js
+++ src/layout/SignInModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   compose,
+  withState,
 } from 'recompose';
 import {
   Button,
@@ -42,15 +43,69 @@ const enchance = compose(
       setRecaptcha,
     },
   ),
+  withState("username", "setUsername", ""),
+  withState("password", "setPassword", ""),
+  withState("usernameAlert", "setUsernameAlert", {
+    status: false,
+    message: ""
+  }),
+  withState("passwordAlert", "setPasswordAlert", {
+    status: false,
+    message: ""
+  }),
 );
 
 const SignInModal = props => {
+  const checkUsername = async () => {
+    if (props.username.length == 0) {
+      props.setUsernameAlert({
+        status: true,
+        message: "Please fill out this field."
+      });
+      return false;
+    } else {
+      props.setUsernameAlert({
+        status: false,
+        message: ""
+      });
+      return true;
+    }
+  }
+
+  const checkPassword = async () => {
+    if (props.password.length == 0) {
+      props.setPasswordAlert({
+        status: true,
+        message: "Please fill out this field."
+      });
+      return false;
+    } else {
+      props.setPasswordAlert({
+        status: false,
+        message: ""
+      });
+      return true;
+    }
+  }
+
   return (
     <Modal isOpen={props.signin.toggleSignIn} toggle={props.toggleSignIn}>
       <ModalHeader>Sign In</ModalHeader>
       <Form onSubmit={async e => {
         e.preventDefault();
-        await props.onSignIn(props.signin.username, props.signin.password);
+        let validators = [];
+        validators.push(await checkUsername());
+        validators.push(await checkPassword());
+        let submit = true;
+        for (let i = 0; i < validators.length; i++) {
+          if (validators[i] === false) {
+            submit = false;
+            break;
+          }
+        }
+        if (submit) {
+          await props.onSignIn(props.signin.username, props.signin.password);
+        }
       }}>
         <ModalBody>
           <FormGroup>
@@ -62,6 +117,11 @@ const SignInModal = props => {
               ) : false
             }
             <Label for="username">Username</Label>
+            <div className="alert alert-danger" role="alert" hidden={!props.usernameAlert.status}>
+              {
+                props.usernameAlert.message
+              }
+            </div>
             <Input
               type="text"
               name="username"
@@ -71,38 +131,55 @@ const SignInModal = props => {
               maxLength={20}
               onChange={e => props.setUsername(e.target.value)}
               disabled={props.signin.loading}
-              required
+              value={props.username}
+              onBlur={() => checkUsername()}
             />
           </FormGroup>
           <FormGroup>
             <Label for="password">Password</Label>
+            <div className="alert alert-danger" role="alert" hidden={!props.passwordAlert.status}>
+              {
+                props.passwordAlert.message
+              }
+            </div>
             <Input
               name="password"
               id="password"
               type="password"
               value={props.signin.password}
-              minLength={8}
-              maxLength={20}
               onChange={e => props.setPassword(e.target.value)}
               disabled={props.signin.loading}
-              required />
+              value={props.password}
+              onBlur={() => checkPassword()}
+            />
+          <div><a href="#">Forget Password</a></div>
           </FormGroup>
           <FormGroup>
-            <Recaptcha 
-               sitekey="6Lc2Nk4UAAAAACAYJLpq3AjyvMkeNFJ9B-dxupUZ"
-               render="explicit"
-               verifyCallback={() => {
+            <Recaptcha
+              sitekey="6Lc2Nk4UAAAAACAYJLpq3AjyvMkeNFJ9B-dxupUZ"
+              render="explicit"
+              verifyCallback={() => {
                 props.setRecaptcha(true);
-               }}
-               expiredCallback={() => {
+              }}
+              expiredCallback={() => {
                 props.setRecaptcha(false);
-               }}
+              }}
             />
           </FormGroup>
         </ModalBody>
         <ModalFooter>
           <Button color="success" type="submit" id="submit" disabled={props.signin.loading || !props.signin.recaptcha}>Sign in</Button>{' '}
-          <Button color="secondary" id="cancel" disabled={props.signin.loading} onClick={props.toggleSignIn}>Cancel</Button>
+          <Button color="secondary" id="cancel" disabled={props.signin.loading} onClick={() => {
+            props.setUsernameAlert({
+              status: false,
+              message: ""
+            });
+            props.setPasswordAlert({
+              status: false,
+              message: ""
+            });
+            props.toggleSignIn(true);
+          }}>Cancel</Button>
         </ModalFooter>
       </Form>
     </Modal>
