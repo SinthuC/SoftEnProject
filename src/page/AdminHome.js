36ae18e04e838829030081e4dffdbf7223b63c23
SinthuC <sinthu_ch@kkumail.com>
2018-04-22 21:52:02 +0700

    Update AdminHome

--- src/page/AdminHome.js
+++ src/page/AdminHome.js
@@ -1,3 +1,5 @@
+
+import axios from 'axios';
 import React from 'react';
 import {
   Container,
@@ -7,47 +9,72 @@ import {
   ListGroupItem,
 } from 'reactstrap';
 import { connect } from 'react-redux';
-import { 
+import {
   compose,
   branch,
   renderComponent,
+  lifecycle,
+  withState,
 } from 'recompose';
 import Page403 from './403';
+import auth from '../redux/action/auth'
+import UserNotice from '../layout/UserNotice'
 
 const styles = {
-  content: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white',
-    height: '90vh',
-  }
-}
+  section: {
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  moreButton: {
+    marginTop: 16,
+    float: 'right',
+  },
+};
 
-const enchance = compose(
-  connect(
-    state => ({
-      auth: state.auth,
-    }),
-    null,
-  ),
+const enchance = compose(connect(
+  state => ({
+    auth: state.auth,
+  }),
+),
   branch(
     props => props.auth.token == null && !props.auth.admin,
     renderComponent(Page403)
-  )
+  ), withState("register", "setRegister",null),
+  lifecycle({
+    componentWillMount() {
+      axios.get(
+        `http://10.199.66.227/SoftEn2018/Sec01_NMB/api/user/readregister.php`
+      ).then(
+        (response) =>{
+          this.props.setRegister(response.data.message);
+        }
+        
+        );
+    }
+  }),
 );
 
 const AdminHome = props => {
   return (
-    <Container fluid style={{ marginTop: 16, marginBottom: 16 }}>
-      <Row>
-        <Col lg={2} md={4} sm={12} xs={12}>
-          
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Container style={styles.section}>
+        <Row style={styles.section}>
+          <Col lg={10} >
+            <h3 style={{ color: 'white' }}>User</h3>
+            <ListGroup>
+              {
+                 props.register!= null ? props.register.map((item, index) => (
+                  <UserNotice id={index} name={item.flname} />
+                )) : <ListGroupItem>
+
+                  </ListGroupItem>
+              }
+            </ListGroup>
+
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
